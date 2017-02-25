c6183c84498d53ba0eea30ff469695c91350d424
baburajnish <baburajnish@gmail.com>
2017-02-25 16:46:55 +0530

    Add Dtail.html and Edit.html

---
+++ School Website/Detail/detail.js
@@ -0,0 +1,26 @@
+﻿window.onload= function(){
+    edit()
+};
+function edit() {
+    var useName=document.getElementById('lblName');
+    useName.innerHTML="Rajnish Tiwari";
+    var email = document.getElementById('lblEmail');
+    email.innerHTML = "baburajnish@gmail.com";
+    var password = document.getElementById('lblPassword');
+    password.innerHTML = "raj1234";
+    var dob = document.getElementById('lblDob');
+    dob.innerHTML = "22-08-1988";
+    var mob = document.getElementById('lblMob');
+    mob.innerHTML = "7205332706";
+    var gender = document.getElementById('lblMale');
+    gender.innerHTML = "Male";
+    var qualifiction = document.getElementById('lblGraduate');
+    qualifiction.innerHTML = "Graduation";
+    var country = document.getElementById('lblCountry');
+    country.innerHTML = "Inda";
+    var address = document.getElementById('lblAddress');
+    address.innerHTML="AT/PO-Brarajnagar(Odisha)"
+
+
+
+}
---
+++ School Website/Edit/edit.js
@@ -0,0 +1,18 @@
+﻿function edit() {
+    var userName = document.getElementById('txtName');
+    userName.value="Rajnish Tiwari";
+    var email = document.getElementById('txtEmail');
+    email.value="baburajnish@gmail.com";
+    var password = document.getElementById('txtPassword');
+    password.value="raj1234";
+    var dob = document.getElementById('txtDob');
+    dob.value= "22-08-1988";
+    var mob = document.getElementById('txtMob');
+    mob.value= "7205332706";
+    var gender = document.getElementById('radioMale');
+    gender.checked = true;
+    var qualifiction = document.getElementById('graduation');
+    qualifiction.checked = true;
+    var address = document.getElementById('txtAddress');
+    address.value= "AT/PO-Brarajnagar(Odisha)"
+}
