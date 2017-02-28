0e7accaff07700074f2a879bff88ef8d27549092
baburajnish <baburajnish@gmail.com>
2017-02-28 20:52:56 +0530

    I created Two Functions  getSelectedStudent and DisplayStudent in detail.js

--- School Website/Detail/detail.js
+++ School Website/Detail/detail.js
@@ -1,4 +1,5 @@
-﻿function showStudent(){
+﻿/*
+function showStudent(){
 var student = new Student();
 student.userName = document.getElementById('lblName');
 student.userName.innerHTML = "Rajnish Tiwari";
@@ -20,12 +21,39 @@ student.address = document.getElementById('lblAddress');
 student.address.innerHTML = "AT/PO-Brarajnagar(Odisha)";
 }
 
-
+*/
 
 
 window.onload = function () {
-    showStudent()
+    DisplayStudent()
 };
+
+
+
+function getSelectedStudent() {
+    var student = new Student("baburajnish@gmail.com", "raj1234", "Rajnish Tiwari",
+        "22-08-1988", "7205332706", "Male", "Graduation", "AT/PO-Brarajnagar(Odisha)", "Inda");
+    return student;
+
+}
+function DisplayStudent() {
+    var student = getSelectedStudent()
+    document.getElementById('lblEmail').innerHTML = student.email;
+    document.getElementById('lblPassword').innerHTML = student.password;
+    document.getElementById('lblName').innerHTML = student.userName;
+    document.getElementById('lblDob').innerHTML = student.dob;
+    document.getElementById('lblMob').innerHTML = student.mob;
+    document.getElementById('lblMale').innerHTML = student.gender;
+    document.getElementById('lblGraduate').innerHTML = student.qualifiction;
+    document.getElementById('lblAddress').innerHTML = student.address;
+    document.getElementById('lblCountry').innerHTML = student.country;
+
+}
+
+
+
+
+
 /*
 function edit() {
    
